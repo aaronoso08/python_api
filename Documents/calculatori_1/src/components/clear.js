65e3497240d1821c9b4603f824c90f74a20d65fd
Aaron <aaronoso08@icloud.com>
2019-01-10 01:59:53 -0700

    made it

--- Documents/calculatori_1/src/components/clear.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const  ClearBtn = props => (
-    <div className="clearBtn" onClick={props.handleClear}>{props.children}</div>
-)
--- Documents/calculatori_1/src/components/input.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const Input = props => <div className="input">{props.input}</div>
